f7090d218693fbdf08c839a9275b8ffb039ec514
Tim Ings <tim@tim-ings.com>
2021-12-15 16:03:27 +0800

    Day 13 part 2

--- day13/main.hs
+++ day13/main.hs
@@ -11,19 +11,24 @@ data FoldLine
 type Coordinate = (Int, Int)
 
 type Case = ([FoldLine], [Coordinate])
-type Soln = Int
+type Soln = Set Coordinate
 
 main :: IO ()
 main = interact handle
 
 handle :: String -> String
-handle = show . solve . readCase
+handle = showPaper . solve . readCase
 
 solve :: Case -> Soln
-solve (foldLines, coordinates) = Set.size foldedPaper
+solve (foldLines, coordinates) = foldl' foldPaper (Set.fromList coordinates) foldLines
+
+showPaper :: Set Coordinate -> String
+showPaper coordinates = unlines [[showSymbol x y | x <- [minimum xs .. maximum xs]] | y <- [minimum ys .. maximum ys]]
   where
-    firstFoldLine = head foldLines
-    foldedPaper = foldPaper firstFoldLine $ Set.fromList coordinates
+    showSymbol x y = if Set.member (x, y) coordinates then '#' else '.'
+    elements = Set.elems coordinates
+    xs = map fst elements
+    ys = map snd elements
 
 readCase :: String -> Case
 readCase raw = foldl' readLine ([], []) (lines raw)
@@ -36,8 +41,8 @@ readLine (foldLines, coordinates) rawLine = (foldLines, coordinates ++ [(read x,
   where
     [x, y] = splitOn "," rawLine
 
-foldPaper :: FoldLine -> Set Coordinate -> Set Coordinate
-foldPaper foldLine = Set.map (foldCoordinate foldLine)
+foldPaper :: Set Coordinate -> FoldLine -> Set Coordinate
+foldPaper coordinates foldLine = Set.map (foldCoordinate foldLine) coordinates
 
 foldCoordinate :: FoldLine -> Coordinate -> Coordinate
 foldCoordinate (VerticalLine lineX) (x, y) = (lineX - abs (x - lineX), y)
