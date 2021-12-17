a93c530a031524cf67e0a819045ce4c334e8c56e
Tim Ings <tim@tim-ings.com>
2021-12-17 18:11:16 +0800

    Adds day 14 part 2

--- day14/main.hs
+++ day14/main.hs
@@ -2,14 +2,14 @@ import Data.List
 import qualified Data.Map as Map
 import Data.Map (Map)
 import Data.Maybe (fromMaybe)
-import Debug.Trace (traceShow)
 import Data.Function (on)
 
 type Polymer = String
 type PolymerPair = (Char, Char)
 type PairInsertionRules = Map PolymerPair Char
+type PolymerPairCounts = Map PolymerPair Int
 
-type Case = (Polymer, PairInsertionRules)
+type Case = (Polymer, PolymerPairCounts, PairInsertionRules)
 type Soln = Int
 
 main :: IO ()
@@ -19,41 +19,55 @@ handle :: String -> String
 handle = show . solve . readCase
 
 solve :: Case -> Soln
-solve (polymer, rules) = mostFrequentCount - lestFrequentCount
+solve (polymer, pairCounts, rules) = mostFrequentCount - leastFrequentCount
   where
-    maturePolymer = growPolymer rules 10 polymer
-    frequencyMap = buildFrequencyMap Map.empty maturePolymer
+    maturePairCounts = growPolymer rules 40 pairCounts
+    frequencyMap = buildFrequencyMap polymer maturePairCounts
     frequencies = sortBy (compare `on` snd) $ Map.toList frequencyMap
-    (_, lestFrequentCount) = head frequencies
+    (_, leastFrequentCount) = head frequencies
     (_, mostFrequentCount) = last frequencies
 
 readCase :: String -> Case
-readCase raw = (polymer, insertionRules)
+readCase raw = (polymer, pairCounts, insertionRules)
   where
     rawLines = lines raw
-    polymer = readPolymer rawLines
+    polymer = head rawLines
+    pairCounts = readPairCounts polymer
     rawInsertionRules = tail $ tail rawLines
     insertionRules = Map.fromList $ map readInsertionRule rawInsertionRules
 
-readPolymer :: [String] -> Polymer
-readPolymer = head
+readPairCounts :: String -> PolymerPairCounts
+readPairCounts polymer = Map.fromListWith (+) $ zip pairs (repeat 1)
+  where
+    pairs = zip polymer (tail polymer)
 
 readInsertionRule :: String -> ((Char, Char), Char)
 readInsertionRule (a : b : ' ':'-':'>':' ' : c : _) = ((a, b), c)
 readInsertionRule _ = error "Failed to parse insertion rule"
 
-growPolymer :: PairInsertionRules -> Int -> Polymer -> Polymer
-growPolymer rules 0 polymer = polymer
-growPolymer rules n polymer = growPolymer rules (pred n) (expandPairs (head polymer) rules (zip polymer $ tail polymer))
+growPolymer :: PairInsertionRules -> Int -> PolymerPairCounts -> PolymerPairCounts
+growPolymer _ 0 pairCounts = pairCounts
+growPolymer rules n  pairCounts = growPolymer rules (pred n) (expandPairs rules pairCounts)
 
-expandPairs :: Char -> PairInsertionRules -> [PolymerPair] -> Polymer
-expandPairs firstChar rules = foldl' (\acc pair -> acc ++ expandPair rules pair) [firstChar]
-
-expandPair :: PairInsertionRules -> PolymerPair -> Polymer
-expandPair rules (a, b) = c : [b]
+expandPairs :: PairInsertionRules -> PolymerPairCounts -> PolymerPairCounts
+expandPairs rules pairCounts = Map.unionWith (+) leftExpandedPairs rightExpandedPairs
   where
-    c = fromMaybe (error "Failed to find rule for pair") $ Map.lookup (a, b) rules
+    leftExpandedPairs = Map.mapKeysWith (+) (expandLeft rules) pairCounts
+    rightExpandedPairs = Map.mapKeysWith (+) (expandRight rules) pairCounts
 
-buildFrequencyMap :: Map Char Int -> Polymer -> Map Char Int
-buildFrequencyMap frequencies [] = frequencies
-buildFrequencyMap frequencies polymer = buildFrequencyMap (Map.insertWith (+) (head polymer) 1 frequencies) (tail polymer)
+expandLeft :: PairInsertionRules -> PolymerPair -> PolymerPair
+expandLeft rules (left, right) = (unwrap $ Map.lookup (left, right) rules, right)
+
+expandRight :: PairInsertionRules -> PolymerPair -> PolymerPair
+expandRight rules (left, right) = (left, unwrap $ Map.lookup (left, right) rules)
+
+unwrap :: Maybe a -> a
+unwrap = fromMaybe (error "Failed to unwrap maybe")
+
+buildFrequencyMap :: Polymer -> PolymerPairCounts -> Map Char Int
+buildFrequencyMap polymer pairCounts = deduplicateCounts rawElementCounts
+  where
+    fstCounts = Map.mapKeysWith (+) fst pairCounts
+    sndCounts = Map.mapKeysWith (+) snd pairCounts
+    rawElementCounts = Map.unionWith (+) fstCounts sndCounts
+    deduplicateCounts = Map.adjust succ (head polymer) . Map.adjust succ (last polymer) . Map.map (`div` 2)
